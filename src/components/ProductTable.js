0a4c6ca180f0f7b2c8b9b1ef9f272122acd351f9
Your Name <shahrizodazayniddinova7@gmail.com>
2025-07-28 23:56:12 +0500

    added responsive

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -3,30 +3,32 @@ import products from "../data/products";
 
 function ProductTable() {
   return (
-    <table className="product-table">
-      <thead>
-        <tr>
-          <th>Название</th>
-          <th>Категория</th>
-          <th>Единица измерения</th>
-          <th>Базовая цена</th>
-          <th>Описание</th>
-          <th>Артикул GTIN</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map((item, index) => (
-          <tr key={index}>
-            <td>{item.name}</td>
-            <td>{item.category}</td>
-            <td>{item.unit}</td>
-            <td>{item.price}</td>
-            <td>{item.description}</td>
-            <td>{item.gtin}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="table-scroll">
+        <table className="product-table">
+        <thead>
+            <tr>
+            <th>Название</th>
+            <th>Категория</th>
+            <th>Единица измерения</th>
+            <th>Базовая цена</th>
+            <th>Описание</th>
+            <th>Артикул GTIN</th>
+            </tr>
+        </thead>
+        <tbody>
+            {products.map((item, index) => (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.category}</td>
+                <td>{item.unit}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>{item.gtin}</td>
+            </tr>
+            ))}
+        </tbody>
+        </table>
+    </div>
   );
 }
 
